35099cb0e9091e12a7bde6708e26c09585857aa0
Jhustyn Carvajal <christopherarias2002@gmail.com>
2025-06-19 17:07:02 -0500

    feat: store password helper functions

---
+++ src/shared/helpers/hashPassword.helper.ts
@@ -0,0 +1,14 @@
+import {hash}  from "@node-rs/argon2";
+
+export async function createHash(password: string): Promise<string>{
+    const options = {
+        type: "aragon2id" as const,
+        memoryCost: 19 * 1024, //? 19 MiB
+        timeCost: 2,
+        parallelism: 1,
+        hashLength: 12
+    };
+
+    return await hash(password, options)
+}  
+
